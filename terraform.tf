405b879db1238954d47838024f7cba6b5f6831b2
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2022-09-03 21:45:43 +0100

    fix: backend

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,12 @@
 terraform {
   required_version = "~> 1.2.0"
   backend "azurerm" {
-    storage_account_name = "tfstatemattfff"
     container_name       = "tfstate"
     key                  = "mattfff.tfstate"
-    use_oidc             = true
+    storage_account_name = "tfstatemattfff"
     subscription_id      = "0a8501c6-55c7-4826-9252-9823c6f839a1"
     tenant_id            = "c180f90e-b05a-4e58-81a2-3f6f7d9340e1"
+    use_azuread_auth     = true
+    use_oidc             = true
   }
 }
