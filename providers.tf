1082a53bd636fbe6ac68ba6f587d91e3d9f72907
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2022-09-03 21:48:10 +0100

    style: fmt

--- providers.tf
+++ providers.tf
@@ -5,12 +5,12 @@ provider "azurerm" {
 
 provider "azurerm" {
   features {}
-  alias = "management"
+  alias           = "management"
   subscription_id = "0a8501c6-55c7-4826-9252-9823c6f839a1"
 }
 
 provider "azurerm" {
   features {}
-  alias = "connectivity"
+  alias           = "connectivity"
   subscription_id = "8b0bdc29-f686-4d2b-aa3f-4c2b509af786"
 }
