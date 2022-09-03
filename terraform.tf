0a5bfe153c2bab723d076e3c19f752058736338d
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2022-09-03 20:46:50 +0100

    feat: use_oidc

--- terraform.tf
+++ terraform.tf
@@ -4,9 +4,8 @@ terraform {
     storage_account_name = "tfstatemattfff"
     container_name       = "tfstate"
     key                  = "mattfff.tfstate"
-    use_azuread_auth     = true
+    use_oidc             = true
     subscription_id      = "0a8501c6-55c7-4826-9252-9823c6f839a1"
     tenant_id            = "c180f90e-b05a-4e58-81a2-3f6f7d9340e1"
-    use_microsoft_graph  = true
   }
 }
