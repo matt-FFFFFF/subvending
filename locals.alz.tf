d349060435fc0651d5f3d6bb1b4d26d85eba5d1a
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-01-13 09:48:48 +0000

    tflint errors

--- locals.alz.tf
+++ locals.alz.tf
@@ -1,6 +1,6 @@
-locals {
-  hub_networks_by_location = {
-    for i, v in module.alz.azurerm_virtual_network.connectivity :
-    v.location => v
-  }
-}
+# locals {
+#   hub_networks_by_location = {
+#     for i, v in module.alz.azurerm_virtual_network.connectivity :
+#     v.location => v
+#   }
+# }
--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,19 @@
 terraform {
   required_version = "~> 1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.8.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.3.0"
+    }
+  }
   backend "azurerm" {
     container_name       = "tfstate"
     key                  = "mattfff.tfstate"
