e7faf2d7eda1a78eb8be426e8e18d56cea799d1c
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-01-13 11:35:46 +0000

    simplify config due to optional

--- main.alz.tf
+++ main.alz.tf
@@ -43,42 +43,13 @@ module "alz" {
   // Management resources
   deploy_management_resources = true
   configure_management_resources = {
-    advanced = null
-    location = ""
     settings = {
       log_analytics = {
-        config = {
-          enable_monitoring_for_arc                         = true
-          enable_monitoring_for_vm                          = true
-          enable_monitoring_for_vmss                        = true
-          enable_sentinel                                   = true
-          enable_solution_for_agent_health_assessment       = true
-          enable_solution_for_anti_malware                  = true
-          enable_solution_for_azure_activity                = true
-          enable_solution_for_change_tracking               = true
-          enable_solution_for_service_map                   = true
-          enable_solution_for_sql_advanced_threat_detection = true
-          enable_solution_for_sql_assessment                = true
-          enable_solution_for_sql_vulnerability_assessment  = true
-          enable_solution_for_updates                       = true
-          enable_solution_for_vm_insights                   = true
-          retention_in_days                                 = 30
-        }
         enabled = true
       }
       security_center = {
         config = {
-          email_security_contact             = "admin@mattfff.onmicrosoft.com"
-          enable_defender_for_app_services   = true
-          enable_defender_for_arm            = true
-          enable_defender_for_containers     = true
-          enable_defender_for_dns            = true
-          enable_defender_for_key_vault      = true
-          enable_defender_for_oss_databases  = true
-          enable_defender_for_servers        = true
-          enable_defender_for_sql_server_vms = true
-          enable_defender_for_sql_servers    = true
-          enable_defender_for_storage        = true
+          email_security_contact = "admin@mattfff.onmicrosoft.com"
         }
         enabled = true
       }
@@ -89,124 +60,29 @@ module "alz" {
   // Connectivity (hub network) configuration
   deploy_connectivity_resources = false
   configure_connectivity_resources = {
-    advanced = null
-    location = ""
     settings = {
       ddos_protection_plan = {
-        config = {
-          location = ""
-        }
         enabled = false
       }
       dns = {
-        config = {
-          enable_private_dns_zone_virtual_network_link_on_hubs   = false
-          enable_private_dns_zone_virtual_network_link_on_spokes = false
-          enable_private_link_by_service = {
-            azure_app_configuration_stores       = false
-            azure_automation_dscandhybridworker  = false
-            azure_automation_webhook             = false
-            azure_backup                         = false
-            azure_cache_for_redis                = false
-            azure_container_registry             = false
-            azure_cosmos_db_cassandra            = false
-            azure_cosmos_db_gremlin              = false
-            azure_cosmos_db_mongodb              = false
-            azure_cosmos_db_sql                  = false
-            azure_cosmos_db_table                = false
-            azure_data_factory                   = false
-            azure_data_factory_portal            = false
-            azure_data_lake_file_system_gen2     = false
-            azure_database_for_mariadb_server    = false
-            azure_database_for_mysql_server      = false
-            azure_database_for_postgresql_server = false
-            azure_event_grid_domain              = false
-            azure_event_grid_topic               = false
-            azure_event_hubs_namespace           = false
-            azure_file_sync                      = false
-            azure_iot_hub                        = false
-            azure_key_vault                      = false
-            azure_kubernetes_service_management  = false
-            azure_machine_learning_workspace     = false
-            azure_monitor                        = false
-            azure_relay_namespace                = false
-            azure_search_service                 = false
-            azure_service_bus_namespace          = false
-            azure_site_recovery                  = false
-            azure_sql_database_sqlserver         = false
-            azure_synapse_analytics_sql          = false
-            azure_synapse_analytics_sqlserver    = false
-            azure_web_apps_sites                 = false
-            cognitive_services_account           = false
-            signalr                              = false
-            storage_account_blob                 = false
-            storage_account_file                 = false
-            storage_account_queue                = false
-            storage_account_table                = false
-            storage_account_web                  = false
-          }
-          location               = ""
-          private_dns_zones      = []
-          private_link_locations = []
-          public_dns_zones       = []
-        }
         enabled = false
       }
       hub_networks = [{
         config = {
           address_space = ["10.0.0.0/23"]
           azure_firewall = {
-            config = {
-              address_prefix = ""
-              availability_zones = {
-                zone_1 = false
-                zone_2 = false
-                zone_3 = false
-              }
-              base_policy_id                = ""
-              dns_servers                   = []
-              enable_dns_proxy              = false
-              private_ip_ranges             = []
-              sku_tier                      = ""
-              threat_intelligence_allowlist = []
-              threat_intelligence_mode      = ""
-            }
             enabled = false
           }
-          bgp_community                           = ""
-          dns_servers                             = []
-          enable_hub_network_mesh_peering         = false
-          enable_outbound_virtual_network_peering = false
-          link_to_ddos_protection_plan            = false
-          location                                = ""
-          spoke_virtual_network_resource_ids      = []
           subnets = [{
-            address_prefixes          = ["10.0.0.0/24"]
-            name                      = "default"
-            network_security_group_id = ""
-            route_table_id            = ""
+            address_prefixes = ["10.0.0.0/24"]
+            name             = "default"
           }]
           virtual_network_gateway = {
-            config = {
-              address_prefix = ""
-              advanced_vpn_settings = {
-                active_active                    = false
-                bgp_settings                     = []
-                custom_route                     = []
-                default_local_network_gateway_id = ""
-                enable_bgp                       = false
-                private_ip_address_allocation    = ""
-                vpn_client_configuration         = []
-              }
-              gateway_sku_expressroute = "value"
-              gateway_sku_vpn          = "value"
-            }
             enabled = false
           }
         }
         enabled = true
       }]
-      vwan_hub_networks = []
     }
     tags = null
   }
